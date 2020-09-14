21f12cafe5e8cb749a8e79bcce40951cb9750333
EC2 Default User <ec2-user@ip-172-31-76-156.ec2.internal>
2020-09-14 07:37:16 +0000

    [Add] サインアップ時のバリデーションチェック処理の追加。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :authentication_keys => [:name]
+         
+  validates :name, presence: true, length: { in: 2..20 } 
 end
