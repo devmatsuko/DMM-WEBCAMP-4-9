7d82f6b25d603198af864f81f5e6cfddc14e0f9a
EC2 Default User <ec2-user@ip-172-31-76-156.ec2.internal>
2020-09-14 09:46:59 +0000

    [Add] Bookモデルの作成および部分テンプレートの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    
+    @user = User.find(params[:id])
+    @book = Book.new
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
