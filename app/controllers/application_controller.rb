582b67b01cd085cd828eb113913e2c2244fcc9f9
EC2 Default User <ec2-user@ip-172-31-76-156.ec2.internal>
2020-09-14 07:22:22 +0000

    [Add] ログイン時、サインアップ時にマイページに遷移する処理の追加。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログイン後はマイページへ遷移するようにルーティングを変更
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  # サインアップ後はマイページへ遷移するようにルーティングを変更
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
+
   protected
 
-# SignUp時にnameフィールドのデータ操作を許可する
+  # サインアップ時、ログイン時にnameフィールドのデータ操作を許可する
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :users
+  resources :users, only: [:index,:show,:edit,]
+  resources :books, :except => :new
   root 'home#index'
   get 'home/about' => 'home#show', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
